1c39e8eacdd3a9d253fa9dfef8a5a0ae0ad34f97
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-06 20:08:18 +0530

    add new project

---
+++ 2pra.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+ printf("Dhruti Viradiya\n");
+ printf("25CS113\n");
+
+ int length=70,breadth=90;
+ int area,perimeter;
+
+ area=length*breadth;
+perimeter=2*(length+breadth);
+ printf("Area=%d\n",area);
+ printf("Perimeter=%d\n",perimeter);
+ }
