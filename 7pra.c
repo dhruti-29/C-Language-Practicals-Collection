82e84c3d466fdd0b305796b49897da233438535b
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-07 18:28:17 +0530

    add new

---
+++ 7pra.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+ printf("Dhruti Viradiya\n");
+ printf("25CS113\n");
+ int a;
+ printf("ENTER YOUR AGE:");
+ scanf("%d",&a);
+if(a<=12) { printf("free ticket");
+
+ } else if(a>60) { printf("free ticket"); }
+ else
+{
+printf("Normal ticket\n");
+printf("prize=150Rs."); } }
