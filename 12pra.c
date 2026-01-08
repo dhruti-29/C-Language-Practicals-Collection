4b2f3097265626c15d29aa0ec60ef98a2d636099
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-08 14:52:21 +0530

    add new file

---
+++ 12pra.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+ printf("Dhruti Viradiya\n");
+ printf("25CS113\n");
+ int i;
+for(i=1;i<=50;i++){
+if(i%5==0)
+ printf("\nBook ID:%d (special edition)",i);
+
+
+ else
+ printf("\nBook ID:%d",i);
+}
+ }
