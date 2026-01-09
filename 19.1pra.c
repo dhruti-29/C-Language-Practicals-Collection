5074dd55fe27fba0abc3904464ba2eb5b78583b6
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-09 20:59:14 +0530

    add new

---
+++ 19.1pra.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+ printf("Dhruti Viradiya\n");
+ printf("25CS113\n");
+ int i,j,n;
+printf("enter number of rows you want=");
+ scanf("%d",&n);
+for(i=1;i<=n;i++) {
+for(j=1;j<=i;j++) {
+
+ printf("%d",(j%2)); 
+
+
+} printf("\n");
+
+} 
+
+}
