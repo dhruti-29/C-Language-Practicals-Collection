d32321f8ed08f58a7ca61ef9d48b5d250eeacfc2
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-09 20:56:57 +0530

    add new

---
+++ 15pra.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+ printf("Dhruti Viradiya\n");
+ printf("25CS113\n");
+ int time;
+printf("enter the time for the timer=");
+ scanf("%d",&time);
+while(time>0)
+ {printf("%d\n",time);
+ time--;
+ }
+
+printf("Countdown Complete!");
+}
