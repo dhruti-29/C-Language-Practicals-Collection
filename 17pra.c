825a0d2212413d3bd27e697864e07cede898bff6
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-09 20:57:46 +0530

    add new

---
+++ 17pra.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main() {
+ printf("Dhruti Viradiya\n");
+ printf("25CS113\n");
+
+ float d=0;
+ int i;
+for(i=1;i<=20&&d<=10;i++)
+ {
+
+ d=d+(0.5);
+ printf("\nMinute %d:Distance covered=%.2f",i,d);
+}
+ printf("\nmarathon complete!");
+ }
