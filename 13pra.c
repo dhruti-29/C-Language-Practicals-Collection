7c913265395633d0cbc19c35d27ab16985cebfe0
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-09 20:56:14 +0530

    add new

---
+++ 13pra.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+ printf("Dhruti Viradiya\n");
+ printf("25CS113\n");
+ int i;
+ printf("water level:");
+ for(i=10;i<100;i=i+10)
+
+ {
+ printf("\nCurrent Water level:%d Liters",i);
+
+}
+printf("\nwater tank is full!");
+}
